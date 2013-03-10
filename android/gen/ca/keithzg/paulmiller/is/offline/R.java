161bb4494fbb90982d59bb42c4f63a1974bbd402
keithzg <keithzg@gmail.com>
2013-03-10 14:30:31 -0600

    Seems like Fragments combined with ViewPager are the way to get slide-able sets of heterogeneous views going; lets see how this naïve assumption comes back to bite me later!

--- android/gen/ca/keithzg/paulmiller/is/offline/R.java
+++ android/gen/ca/keithzg/paulmiller/is/offline/R.java
@@ -26,12 +26,14 @@ public final class R {
         public static final int pauloff_480p=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
-        public static final int myImageView=0x7f090000;
+        public static final int action_settings=0x7f090002;
+        public static final int myImageView=0x7f090001;
+        public static final int viewpager=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int paulwidget=0x7f030001;
+        public static final int main=0x7f030001;
+        public static final int paulwidget=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
